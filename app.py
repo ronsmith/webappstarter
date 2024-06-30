74fa7540184a6068dd2758d77af7a376447d4aef
Ron Smith <ron.smith@thataintworking.com>
2024-06-30 16:08:17 -0400

    base template with index working

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, session, request, redirect, url_for
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+app.secret_key = b'c61551d364ddd8d42775eb00796f501092b14d1378e60a6980c31e49ccffe80a'
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        pass  # TODO: handle login
+    # else of fall-thru
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ static/base.js
@@ -0,0 +1,4 @@
+function nav_set_selected(label) {
+    // TODO set the specified nav option as selected
+}
+
