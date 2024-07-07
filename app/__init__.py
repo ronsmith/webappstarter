fdad221ce096dde6da3fe7913618e24906f49164
Ron Smith <ron.smith@thataintworking.com>
2024-07-07 18:08:12 -0400

    dbsession is working

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template, session, request, redirect, url_for
-from dotenv import load_dotenv
+import logging
+import os
 
-from app.dbsession import DBSessionInterface
-from db import DBPool
+from flask import Flask, render_template, session, request, redirect, url_for, send_from_directory, make_response
+from dotenv import load_dotenv
+from .dbsession import DBSessionInterface
+from .db import DBPool
 
 load_dotenv()
+
+logging.basicConfig(level=os.environ.get("LOGLEVEL", logging.WARNING))
+_logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 app.secret_key = b'c61551d364ddd8d42775eb00796f501092b14d1378e60a6980c31e49ccffe80a'
@@ -15,6 +20,15 @@ app.session_interface = DBSessionInterface()
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, 'static'),
+        'favicon.ico',
+        mimetype='image/vnd.microsoft.icon'
+    )
 
 
 @app.route('/login', methods=['GET', 'POST'])
--- app/dbsession.py
+++ app/dbsession.py
@@ -1,8 +1,12 @@
 import os
-from db import DatabaseError
+import logging
+import json
+from .db import DatabaseError
 from flask.sessions import SessionInterface, SessionMixin
 
 _DEFAULT_SESSION_DURATION = '30 MINUTES'
+
+_logger = logging.getLogger(__name__)
 
 
 class Session(dict, SessionMixin):
@@ -15,32 +19,62 @@ class Session(dict, SessionMixin):
         if key_vals:
             self.update(key_vals)
 
+    def replace_key_vals(self, new_key_vals):
+        self.clear()
+        self.update(new_key_vals)
+
 
 class DBSessionInterface(SessionInterface):
 
-    def open_session(self, app, request) -> Session:
+    def open_session(self, app, request) -> Session | None:
+        if request.path.startswith('/static') or request.path.startswith('/favicon'):
+            return None
+
         session_id = request.cookies.get(app.config['SESSION_COOKIE_NAME'])
+        _logger.debug(f'got request cookie {app.config['SESSION_COOKIE_NAME']}={session_id}')
+
         s = None
         conn = None
+
         try:
             conn = app.db_pool.getconn()
             clean_expired_sessions(conn)  # could be run out of band in a separate process
-            with conn.cursor() as cursor:
-                if session_id:
-                    cursor.execute("SELECT userid, expires, key_vals FROM sessions WHERE id = %s", (session_id,))
-                    row = cursor.fetchone()
-                    if row:
-                        s = Session(session_id, row[0], row[1], row[2])
+            with conn:
+                with conn.cursor() as cursor:
+                    if session_id:
+                        cursor.execute("""
+                            SELECT expires, userid, key_vals 
+                            FROM sessions 
+                            WHERE id = %s
+                        """, (session_id,))
+                        row = cursor.fetchone()
+                        if row:
+                            s = Session(session_id, expires=row[0], userid=row[1])
+                            s.replace_key_vals(row[2])
+                            _logger.debug(f'opening existing session ID {s.id}')
+                        else:
+                            _logger.debug(f'No session found for ID {session_id}')
 
-                if not s:  # no session_id or session_id returns no rows
-                    sesdur = os.environ.get(app.config['SESSION_COOKIE_NAME'], _DEFAULT_SESSION_DURATION)
-                    cursor.execute("INSERT INTO sessions (expires) VALUES (now() + %s) RETURNING id, expires", (sesdur,))
-                    row = cursor.fetchone()
-                    if row:
-                        s = Session(row[0], row[1])
+                    # s will be None if no session_id or session_id returns no rows
+                    # Must use "is None" because "not s" returns true when the dict is empty
+                    if s is None:
+                        sesdur = os.environ.get(app.config['SESSION_COOKIE_NAME'], _DEFAULT_SESSION_DURATION)
+                        cursor.execute("""
+                            INSERT INTO sessions (expires) 
+                                VALUES (now() + INTERVAL %s) 
+                                RETURNING id, expires
+                        """, (sesdur,))
+                        row = cursor.fetchone()
+                        if row:
+                            s = Session(id=row[0], expires=row[1])
+                            _logger.debug(f'opening new session with ID {s.id}')
+                        else:
+                            raise DatabaseError("No row returned from execute")
         finally:
             if conn:
                 app.db_pool.putconn(conn)
+
+        _logger.debug(f'open_session returning session ID {s.id}')
         return s
 
     def save_session(self, app, session: Session, response):
@@ -49,44 +83,51 @@ class DBSessionInterface(SessionInterface):
             'id': session.id,
             'sesdur': os.environ.get(app.config['SESSION_COOKIE_NAME'], _DEFAULT_SESSION_DURATION),
             'userid': session.userid,
-            'key_vals': session.items()
+            'key_vals': json.dumps(session)
         }
         try:
-            conn = app.db_pool.getconn()
-            with conn.cursor() as cursor:
+            with app.db_pool.getconn() as conn:
+                with conn.cursor() as cursor:
 
-                if session.id:
-                    cursor.execute("""
-                        INSERT INTO sessions (id, expires, userid, key_vals)
-                            VALUES (%(id)s, now() + %(sesdur)s, %(userid)s, %(key_vals)s)
-                        ON CONFLICT DO UPDATE SET
-                            expires = now() + %(sesdur)s,
-                            userid = %(userid)s,
-                            key_vals = %(key_vals)s
-                            WHERE id = %(id)s
-                        RETURNING expires
-                    """, params)
-                    row = cursor.fetchone()
-                    if row:
-                        session.expires = row[0]
-                    else:
-                        raise DatabaseError('No row returned from execute')
+                    if session.id:
+                        cursor.execute("""
+                            INSERT INTO sessions (id, expires, userid, key_vals)
+                                VALUES (%(id)s, now() + %(sesdur)s, %(userid)s, %(key_vals)s)
+                            ON CONFLICT (id) DO UPDATE SET
+                                expires = now() + INTERVAL %(sesdur)s,
+                                userid = %(userid)s,
+                                key_vals = %(key_vals)s
+                            RETURNING expires
+                        """, params)
+                        row = cursor.fetchone()
+                        if row:
+                            session.expires = row[0]
+                            _logger.debug(f'saved existing session ID {session.id}')
+                        else:
+                            raise DatabaseError('No row returned from execute')
 
-                else:  # session doesn't have an id (unlikely)
-                    cursor.execute("""
-                        INSERT INTO sessions (expires, userid, key_vals)
-                            VALUES (now() + %(sesdur)s, %(userid)s, %(key_vals)s)
-                            RETURNING id, expires
-                    """, params)
-                    row = cursor.fetchone()
-                    if row:
-                        session.id = row[0]
-                        session.expires = row[1]
-                    else:
-                        raise DatabaseError('No row returned from execute')
+                    else:  # session doesn't have an id (unlikely)
+                        cursor.execute("""
+                            INSERT INTO sessions (expires, userid, key_vals)
+                                VALUES (now() + INTERVAL %(sesdur)s, %(userid)s, %(key_vals)s)
+                                RETURNING id, expires
+                        """, params)
+                        row = cursor.fetchone()
+                        if row:
+                            session.id = row[0]
+                            session.expires = row[1]
+                            _logger.debug(f'saved new session ID {session.id}')
+                        else:
+                            raise DatabaseError('No row returned from execute')
 
             # Always set the session cookie with the updated epiration if we made it this far
-            response.set_cookie(app.config['SESSION_COOKIE_NAME'], session.id, expires=session.expires, httponly=True)
+            response.set_cookie(
+                key=app.config['SESSION_COOKIE_NAME'],
+                value=session.id,
+                httponly=True,
+                secure=False,   # should be secure in production
+                samesite='Lax')
+            _logger.debug(f'set response cookie {app.config['SESSION_COOKIE_NAME']}={session.id}')
 
         finally:
             if conn:
@@ -94,5 +135,6 @@ class DBSessionInterface(SessionInterface):
 
 
 def clean_expired_sessions(conn):
-    with conn.cursor() as cursor:
-        cursor.execute("DELETE FROM sessions WHERE expires > now()")
+    with conn:
+        with conn.cursor() as cursor:
+            cursor.execute("DELETE FROM sessions WHERE now() > expires")
