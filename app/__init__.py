6d34198ea78d6c573b8d7ab14ab2966791308cd8
Ron Smith <ron.smith@thataintworking.com>
2024-07-08 22:08:52 -0400

    fleshed out login functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 import logging
 import os
 
-from flask import Flask, render_template, session, request, redirect, url_for, send_from_directory, make_response
+from flask import Flask, render_template, session, request, redirect, url_for, send_from_directory, make_response, flash
 from dotenv import load_dotenv
+
+from .remember import get_remember_me, set_remember_me
 from .dbsession import DBSessionInterface
 from .db import DBPool
+from .user import login_user
 
 load_dotenv()
 
@@ -14,7 +17,7 @@ _logger = logging.getLogger(__name__)
 app = Flask(__name__)
 app.secret_key = b'c61551d364ddd8d42775eb00796f501092b14d1378e60a6980c31e49ccffe80a'
 app.db_pool = DBPool()
-app.session_interface = DBSessionInterface()
+app.session_interface = DBSessionInterface(app)
 
 
 @app.route('/')
@@ -34,9 +37,17 @@ def favicon():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        pass
-    # else of fall-thru
-    return render_template('login.html')
+        u = user.login(app.db_pool.getconn(), request.form['email'], request.form['password'])
+        if u:
+            resp = make_response(redirect(url_for(request.args.get('next', 'index'))))
+            session.userid = u.id
+            set_remember_me(request, resp)
+
+        else:
+            flash('Invalid email or password', category='error')
+
+    # else fall-thru
+    return render_template('login.html', remember=get_remember_me(request))
 
 
 @app.route('/logout')
